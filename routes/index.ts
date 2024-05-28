a1a2c48c4dbeea6dad7e6470eab47852c43c6216
MSI\gn01792218 <gn01792218@hotmail.com>
2024-05-28 12:12:11 +0800

    [tity up]

--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express'
 import userRoute from './user'
 import roleRoute from './role'
-import vocabulary from './vocabulary'
 
 const router = Router()
 
 router.use(userRoute)
 router.use(roleRoute)
-router.use(vocabulary)
 
 export default router
